39c2838ab1b83368c52896fca44351b345970b95
LordAur <yudha.webdev@gmail.com>
2021-03-08 22:43:33 +0700

    Update client_test to increase the code coverage

--- test/client_test.go
+++ test/client_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httptest"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/KodepandaID/panggilhttp"
 	"github.com/stretchr/testify/assert"
@@ -223,4 +224,225 @@ func TestMethodPOSTWithSendFile(t *testing.T) {
 	if resp.StatusCode != http.StatusOK {
 		t.Fatal("HTTP request failed")
 	}
+}
+
+func TestMethodPUT(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			t.Errorf("Expected ‘PUT’ request, got ‘%s’", r.Method)
+		}
+
+		body, e := ioutil.ReadAll(r.Body)
+		if e != nil {
+			t.Fatal(e)
+		}
+
+		c := make(map[string]interface{})
+		json.Unmarshal(body, &c)
+
+		if c["username"] != "administrator" || c["password"] != "password" {
+			w.WriteHeader(http.StatusBadRequest)
+			t.Error("Request body not match")
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer ts.Close()
+
+	client := panggilhttp.New()
+
+	resp, e := client.
+		Put(ts.URL).
+		SendJSON(map[string]interface{}{
+			"username": "administrator",
+			"password": "password",
+		}).
+		Do()
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("HTTP request failed")
+	}
+}
+
+func TestMethodPATCH(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPatch {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			t.Errorf("Expected ‘PATCH’ request, got ‘%s’", r.Method)
+		}
+
+		body, e := ioutil.ReadAll(r.Body)
+		if e != nil {
+			t.Fatal(e)
+		}
+
+		c := make(map[string]interface{})
+		json.Unmarshal(body, &c)
+
+		if c["username"] != "administrator" || c["password"] != "password" {
+			w.WriteHeader(http.StatusBadRequest)
+			t.Error("Request body not match")
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer ts.Close()
+
+	client := panggilhttp.New()
+
+	resp, e := client.
+		Patch(ts.URL).
+		SendJSON(map[string]interface{}{
+			"username": "administrator",
+			"password": "password",
+		}).
+		Do()
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("HTTP request failed")
+	}
+}
+
+func TestMethodDELETE(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodDelete {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			t.Errorf("Expected ‘DELETE’ request, got ‘%s’", r.Method)
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer ts.Close()
+
+	client := panggilhttp.New()
+
+	resp, e := client.
+		Delete(ts.URL).
+		Do()
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("HTTP request failed")
+	}
+}
+
+func TestRequestTimeout(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2 * time.Second)
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "application/json")
+		w.Write([]byte(`{"message":"ping"}`))
+	}))
+	defer ts.Close()
+
+	client := panggilhttp.New()
+
+	_, e := client.
+		Get(ts.URL, nil, nil).
+		Do()
+
+	assert.Equal(t, e.Error(), "Request Timeout")
+}
+
+func TestWithHeader(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Authorization")
+		if auth != "123456" {
+			w.WriteHeader(http.StatusUnauthorized)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "application/json")
+		w.Write([]byte(`{"message":"ping"}`))
+	}))
+	defer ts.Close()
+
+	client := panggilhttp.New()
+
+	resp, e := client.
+		WithHeader(map[string]string{
+			"Authorization": "123456",
+		}).
+		Get(ts.URL, nil, nil).
+		Do()
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	assert.Equal(t, 200, resp.StatusCode)
+}
+
+func TestWithCookies(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, e := r.Cookie("Authorization")
+		if e != nil {
+			t.Fatal(e)
+		}
+		if cookie.Name != "Authorization" {
+			w.WriteHeader(http.StatusUnauthorized)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "application/json")
+		w.Write([]byte(`{"message":"ping"}`))
+	}))
+	defer ts.Close()
+
+	client := panggilhttp.New()
+
+	resp, e := client.
+		WithCookie(map[string]string{
+			"Authorization": "123456",
+		}).
+		Get(ts.URL, nil, nil).
+		Do()
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	assert.Equal(t, 200, resp.StatusCode)
+}
+
+func TestWithTimeout(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(3 * time.Second)
+	}))
+	defer ts.Close()
+
+	client := panggilhttp.New()
+
+	_, e := client.
+		WithTimeout(2).
+		Get(ts.URL, nil, nil).
+		Do()
+
+	assert.Equal(t, e.Error(), "Request Timeout")
+}
+
+func TestRetryFail(t *testing.T) {
+	attempts := 0
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2 * time.Second)
+		attempts++
+	}))
+	defer ts.Close()
+
+	client := panggilhttp.New()
+
+	client.
+		Get(ts.URL, nil, nil).
+		WithFailRetry(500, 3).
+		Do()
+
+	assert.Equal(t, 3, attempts)
 }
